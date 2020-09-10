412c3d69f9f15c3ea45ceba20d61d3c6cffad2a0
Walter Banda <walterkaimurima@gmail.com>
2020-09-11 00:29:57 +0300

    First Commit 😎

--- src/App.java
+++ src/App.java
@@ -1,11 +1,26 @@
+import recap.one.StringR;
+
 public class App {
     public static void main(String[] args) {
+        space();
+        System.out.println("Main Entry Point");
+        space();
+        welcome();
+        space();
+        System.out.println("Other classes");
+        space();
+        StringR.madlibs();
+    }
+    static void welcome(){
         System.out.print("#####################################################\n" +
-                         "#################--    Hi 👋--    --#################\n" +
-                         "####-- My Java Recap ☕ and Small Applications --####\n" +
-                         "########--  And Some other small projects  --########\n" +
-                         "##############---- Made with 💗--------##############\n" +
-                         "#####################################################"
+                "#################--    Hi 👋      --#################\n" +
+                "####-- My Java Recap ☕ and Small Applications --####\n" +
+                "########--  And Some other small projects  --########\n" +
+                "##############---- Made with 💗--------##############\n" +
+                "#####################################################"
         );
+    }
+    static void space(){
+        System.out.println("\n");
     }
 }
